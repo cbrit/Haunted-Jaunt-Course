955812bb50d933001bc7c56699a746b28ca3650c
Collin Brittain <collinbrittain@gmail.com>
2020-05-07 22:56:02 -0500

    Add all other files, arrow-key movement control

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // radians per second
+    public float turnSpeed = 20f;
+
+    private Rigidbody m_Rigidbody;
+    private Animator m_Animator;
+    private Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+
+        // Keeps diagonal speeds from being higher
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        // "deltaPosition is the change in position due to root motion 
+        // that would have been applied to this frame"
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
